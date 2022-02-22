4f4cc1a7e68d96ad99a32e34bb23eedbf962e55d
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2022-02-22 12:04:38 -0500

    finished html and css, working on script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const targetWords = require("./targetWords.json");
+const dictionary = require("./dictionary.json");
+
+const WORD_LENGTH = 5;
+const FLIP_ANIMATION_DURATION = 500;
+const DANCE_ANIMATION_DURATION = 500;
+const keyboard = document.querySelector("[data-keyboard]");
+const alertContainer = document.querySelector("[data-alert-container]");
+const guessGrid = document.querySelector("[data-guess-grid]");
